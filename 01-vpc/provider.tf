c8a4a3bbabb3618df5d43663bcd1219d1a704e67
Mohammed Abdulah Quraishi <mdabdulahquraish@gmail.com>
2024-02-04 21:41:29 +0300

    pushing roboshop terraform into git

---
+++ 01-vpc/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.31.0"
+    }
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "littlesimba"
+    key    = "vpc"
+    region = "us-east-1"
+    dynamodb_table = "littlsimba_lock"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
